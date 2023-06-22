7e1033e862d7776502c421845b9971583bcb939e
banovshaabdiyeva <abdiyeva.banovsha@gmail.com>
2023-06-22 21:57:53 +0400

    22.06.23 tasks added

--- P517_c#1st/Program.cs
+++ P517_c#1st/Program.cs
@@ -1,4 +1,5 @@
-﻿////1
+﻿using System.Text.RegularExpressions;
+////1
 //int a = 4;
 //int b = 5;
 //int sum = 0;
@@ -137,36 +138,124 @@
 //Console.WriteLine(a);
 
 
-int point = 96;
+//int point = 96;
 
-if (point >= 0 && point <= 100)
-{
-    if (point >= 91 && point <= 100)
-    {
-        Console.WriteLine("A almisiz");
-    }
-    else if (point >= 81 && point <= 90)
-    {
-        Console.WriteLine("B almisiz");
-    }
-    else if (point >= 71 && point <= 80)
-    {
-        Console.WriteLine("C almisiz");
-    }
-    else if (point >= 61 && point <= 70)
-    {
-        Console.WriteLine("D almisiz");
-    }
-    else if (point >= 51 && point <= 60)
-    {
-        Console.WriteLine("E almisiz");
-    }
-    else
-    {
-        Console.WriteLine("Kesildiz");
-    }
-}
-else
-{
-    Console.WriteLine("Duzgun qiymet daxil edin: 0-100 araliginda");
-}
+//if (point >= 0 && point <= 100)
+//{
+//    if (point >= 91 && point <= 100)
+//    {
+//        Console.WriteLine("A almisiz");
+//    }
+//    else if (point >= 81 && point <= 90)
+//    {
+//        Console.WriteLine("B almisiz");
+//    }
+//    else if (point >= 71 && point <= 80)
+//    {
+//        Console.WriteLine("C almisiz");
+//    }
+//    else if (point >= 61 && point <= 70)
+//    {
+//        Console.WriteLine("D almisiz");
+//    }
+//    else if (point >= 51 && point <= 60)
+//    {
+//        Console.WriteLine("E almisiz");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Kesildiz");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Duzgun qiymet daxil edin: 0-100 araliginda");
+//}
+
+
+//22.06.2023
+
+//1
+
+//Regex emailRegex = new Regex(@"^[a-zA-Z][a-zA-Z.0-9_]+@[a-zA-Z]{2,10}\.[a-zA-Z]{2,10}(\.[a-zA-Z]{2,10})?$");
+//Regex passwordRegex = new Regex(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,10}$");
+
+//Console.Write("Email daxil edin: ");
+//string email = Console.ReadLine();
+
+//while (!emailRegex.IsMatch(email))
+//{
+//    Console.Write("Duzgun formatda mail daxil edin: ");
+//    email = Console.ReadLine();
+//}
+//Console.Write("Sifre daxil edin: ");
+//string password = Console.ReadLine();
+//while (!passwordRegex.IsMatch(password))
+//{
+//    Console.Write("Duzgun formatda sifre daxil edin: ");
+//    password = Console.ReadLine();
+//}
+//Console.Write("Sifreni testiq edin: ");
+//string confirmPassword = Console.ReadLine();
+//while (password != confirmPassword)
+//{
+//    Console.Write("Yazdigiz sifreler ust-uste dusmur: ");
+//    confirmPassword = Console.ReadLine();
+//}
+//Console.WriteLine("Ugurla qeydiyyatdan kecildi");
+
+
+//2
+//int guesses = 0; 
+//bool isWon = false; 
+//Console.Write("0-10 arasinda 1 reqem texmin edin: "); 
+
+//while (guesses < 3) 
+//{
+//    int num = Convert.ToInt32(Console.ReadLine());
+//    if (num == 7)
+//    {
+//        isWon = true;
+//        break;
+//    }
+//    else if (num < 7)
+//    {
+//        Console.WriteLine("Boyuk reqem olmalidir:");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Kicik reqem olmalidir:");
+//    }
+
+//    guesses++;
+//}
+
+//if (isWon) 
+//{
+//    Console.WriteLine("Qalibsiz!");
+//}
+//else
+//{
+//    Console.WriteLine("Meglub oldunuz reqem 7 idi");
+//}
+
+//7
+//Console.Write("Eded daxil edin: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int module = 0;
+
+//while (num > 0)
+//{
+//    module = num % 10;
+//    num = (num - module) / 10;
+//    //1
+//    //258 % 10 --> 8 -module
+//    //(258-8) /10 = 25 - num
+//    //2
+//    //25 % 10  --> 5 -module
+//    //(25-5) /10 = 2 - num
+//    //2 % 10 --> 2 -module
+//    //(2-2) / 10 = 0 -num
+//    Console.Write(module +" ,");
+//}
+
